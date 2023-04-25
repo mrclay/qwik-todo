61954519c5e5f88feeb63e8ee45c263c39e91c48
Steve Clay <steve@mrclay.org>
2023-04-25 19:44:10 -0400

    Fix item editing

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,4 +1,9 @@
-import { component$, useContext, useSignal } from "@builder.io/qwik";
+import {
+  component$,
+  useContext,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import type { Todo } from "~/state";
 import { StateCtx } from "~/routes";
 
@@ -11,62 +16,79 @@ export const Item = component$<ItemProps>(({ todo }) => {
   const editing = useSignal(false);
   const editLabel = useSignal("");
 
+  useVisibleTask$(({ track }) => {
+    const isEditing = track(() => editing.value);
+
+    if (isEditing) {
+      const input = document.querySelector<HTMLInputElement>(
+        `#TODO-edit-${todo.id}`
+      );
+      if (input) {
+        input.select();
+      }
+    }
+  });
+
   return (
     <li
       class={`${todo.completed ? "completed" : ""} ${
         editing.value ? "editing" : ""
       }`}
     >
-      <div class="view">
+      {editing.value ? (
         <input
-          class="toggle"
-          type="checkbox"
-          onClick$={() => {
-            store.items = store.items.map(({ id, label, completed }) => ({
-              id,
-              label,
-              completed: id === todo.id ? !completed : completed,
-            }));
+          id={`TODO-edit-${todo.id}`}
+          class="edit"
+          type="text"
+          value={editLabel.value}
+          onChange$={(e) => {
+            editLabel.value = e.target.value;
           }}
-          checked={todo.completed}
-        />
-        <label
-          onDblClick$={() => {
-            editLabel.value = todo.label;
-            editing.value = true;
-          }}
-        >
-          {editing.value && (
-            // TODO Why does this disappear?
-            <input
-              class="edit"
-              type="text"
-              value={editLabel.value}
-              onChange$={(e) => {
-                editLabel.value = e.target.value;
-              }}
-              onKeyUp$={(e) => {
-                if (e.key === "Enter") {
-                  store.items = store.items.map(({ id, completed }) => ({
-                    id,
-                    label: editLabel.value,
-                    completed,
-                  }));
-                  editing.value = false;
-                }
-              }}
-            />
-          )}
-          {!editing.value && <span>{todo.label}</span>}
-        </label>
-        <button
-          type="button"
-          class="destroy"
-          onClick$={() => {
-            store.items = store.items.filter((item) => item.id !== todo.id);
+          onKeyUp$={(e) => {
+            if (e.key === "Enter") {
+              store.items = store.items.map(({ id, label, completed }) => ({
+                id,
+                label: id === todo.id ? editLabel.value : label,
+                completed,
+              }));
+              editing.value = false;
+            }
+            if (e.key === "Escape") {
+              editing.value = false;
+            }
           }}
         />
-      </div>
+      ) : (
+        <div class="view">
+          <input
+            class="toggle"
+            type="checkbox"
+            onClick$={() => {
+              store.items = store.items.map(({ id, label, completed }) => ({
+                id,
+                label,
+                completed: id === todo.id ? !completed : completed,
+              }));
+            }}
+            checked={todo.completed}
+          />
+          <label
+            onDblClick$={() => {
+              editLabel.value = todo.label;
+              editing.value = true;
+            }}
+          >
+            {todo.label}
+          </label>
+          <button
+            type="button"
+            class="destroy"
+            onClick$={() => {
+              store.items = store.items.filter((item) => item.id !== todo.id);
+            }}
+          />
+        </div>
+      )}
     </li>
   );
 });
