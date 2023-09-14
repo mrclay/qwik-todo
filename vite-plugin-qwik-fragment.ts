c38ecab5e2a0a89c06e8d73b3e32a639cd9478d4
Steve Clay <steve@mrclay.org>
2023-09-14 09:06:26 -0400

    Lint errors

--- vite-plugin-qwik-fragment.ts
+++ vite-plugin-qwik-fragment.ts
@@ -21,28 +21,34 @@ export default function vitePluginQwikFragment() {
   };
 }
 
-function extractQwikMarkup(markup) {
+function extractQwikMarkup(markup: string) {
   let styles = '';
-  let htmlAttrs = {};
+  const htmlAttrs: Record<string, string | undefined> = {};
   let body = '';
   let bootScripts = '';
 
-  markup.replace(/<html ([^>]+)>/, (m0, m1) => {
+  markup.replace(/<html ([^>]+)>/, (m0, m1: string) => {
     m1.replace(/(q[^= ]+)(="([^"]*)")?/g, (m0, m1, m2, m3) => {
       htmlAttrs[m1] = m3 ? m3 : '';
+      return m0;
     });
+
+    return m0;
   });
 
   markup.replace(/<style [\w\W]*?<\/style>/g, (m0) => {
     styles += m0;
+    return m0;
   });
 
   markup.replace(/<body .*?>([\w\W]*?)<\/body>/, (m0, m1) => {
     body += m1;
+    return m0;
   });
 
   markup.replace(/<\/body>([\w\W]*?)<\/html>/, (m0, m1) => {
     bootScripts += m1;
+    return m0;
   });
 
   const addHtmlAttrsScript = `
@@ -56,7 +62,7 @@ function extractQwikMarkup(markup) {
       })();
     </script>
   `
-    .replace(/\n    /g, '\n')
+    .replace(/\n {4}/g, '\n')
     .trim();
 
   return {
